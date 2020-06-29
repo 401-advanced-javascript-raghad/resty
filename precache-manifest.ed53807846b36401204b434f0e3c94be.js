eebba396f3430557278a6d9bef505c49a935a7ae
raghadalquran <raghadalquran1@gmail.com>
2020-06-29 18:48:49 +0300

    Updates

---
+++ precache-manifest.ed53807846b36401204b434f0e3c94be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d1c455a9642453319ffb650f4db4f8",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "6857c99400a24805e5af",
+    "url": "/resty/static/css/main.2a68faed.chunk.css"
+  },
+  {
+    "revision": "3c3fe42eb5bed02bd8ed",
+    "url": "/resty/static/js/2.62d548b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.62d548b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6857c99400a24805e5af",
+    "url": "/resty/static/js/main.509f5d9c.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/401-advanced-javascript-raghad/resty/precache-manifest.2b5d8a720e4176566af0fea4f753d070.js"
+  "/resty/precache-manifest.ed53807846b36401204b434f0e3c94be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/401-advanced-javascript-raghad/resty/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resty/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
